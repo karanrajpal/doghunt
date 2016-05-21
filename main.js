9a273f46ef4837d3d4efef7259ed54296b93123c
Karan Rajpal <amigokaran@gmail.com>
2016-05-21 14:08:05 -0400

    functional

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ function DogHunter() {
 		var svgParent = document.getElementById('svgParent');
 		svgParent.appendChild(svg);
 
-		var width = 900;
-		var height = 520;
+		var width = 1000;
+		var height = 500;
 		var margin = {top:50, left:40, bottom:100, right:40};
 
 
@@ -17,4 +17,9 @@ function DogHunter() {
 	function fetchAndSetupData() {
 		// Read 
 	}
+	return {
+		init: init
+	};
 }
+
+DogHunter().init();
